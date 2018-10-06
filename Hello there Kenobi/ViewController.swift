6778806491106bfb0afd7d68f44cce3277398016
Gustav Bang <gustavbang96@gmail.com>
2018-10-06 14:02:39 +0200

    More testing

--- Hello there Kenobi/ViewController.swift
+++ Hello there Kenobi/ViewController.swift
@@ -12,25 +12,45 @@ class ViewController: UIViewController {
 
     var name = "one"
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         var person1 = Person(name: "Absalon")
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
+        let arr = [1, 2, 3, 4, 5, 6, 6, 4, 3, 102, 1, 2, -32, 5]
+        print(findMinMax(arr: arr))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
 
     @IBAction func btnPressed(_ sender: UIButton) {
         let array = ["The Force will be with you. Always.", "There’s always a bigger fish.", "Fear is the path to the dark side. Fear leads to anger; anger leads to hate; hate leads to suffering. I sense much fear in you.", "I’m just a simple man trying to make my way in the universe."]
         let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
         print(array[randomIndex] + "\n")}
     
+    
+    func findMinMax(arr: [Int]) -> (min:Int, max:Int) {
+        var currentMin = arr[0]
+        var currentMax = arr[0]
+        for i in 1..<arr.count {
+            let value = arr[i]
+            if (value > currentMax) {
+                currentMax = value
+            }
+            
+            else if (value < currentMin) {
+                currentMin = value
+            }
+        }
+        return (currentMin, currentMax)
+    }
 }
 
